e5e9dfb2a4b3793afb829c53184d0f24f121d5e7
Peter Njuguna <njugunapm23@gmail.com>
2023-02-25 12:46:33 +0300

    have create and designed a navbar for this gym app that is full responsive on various devices

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,32 @@
-import Navbar from "@/scenes/navbar"
+import { useEffect, useState } from "react";
+import Navbar from "@/scenes/navbar";
+import { SelectedPage } from "@/shared/types";
 
 function App() {
-  return <div className="app bg-gray-20">
-    <Navbar />
-  </div>;
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(
+    SelectedPage.Home
+  );
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true);
+        setSelectedPage(SelectedPage.Home);
+      }
+      if (window.scrollY !== 0) setIsTopOfPage(false);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+  return (
+    <div className="app bg-gray-20">
+      <Navbar
+        isTopOfPage={isTopOfPage}
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
+    </div>
+  );
 }
 
 export default App;
--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -1,30 +1,115 @@
-import React, { useState } from 'react'
-import { bookopenIcon, wrenchscrewdriverIcon } from "@heroicons/react/24/solid";
+import React, { useState } from "react";
+import { BookOpenIcon, ScissorsIcon } from "@heroicons/react/24/solid";
 import Logo from "@/assets/Logo.png";
+import Link from "./Link";
+import { SelectedPage } from "@/shared/types";
+import useMediaQuery from "@/hooks/useMediaQuery";
+import ActionButton from "@/shared/ActionButton";
 
-type Props = {}
+type Props = {
+  isTopOfPage: boolean;
+  selectedPage: SelectedPage;
+  setSelectedPage: (value: SelectedPage) => void;
+};
 
-const flexBetween = "flex items-center justify-between"
+const Navbar = ({ isTopOfPage, selectedPage, setSelectedPage }: Props) => {
+  const flexBetween = "flex items-center justify-between";
+  const isAboveMediumScreens = useMediaQuery("(min-width:1060px)");
+  const [isMenuToggled, setIsMenuToggled] = useState<boolean>(false);
+  const navbarBackground = isTopOfPage ? "" : "bg-primary-100 drop-shadow";
 
-const Navbar = (props: Props) => {
-    return (
-        <nav>
-            <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
-                <div className={`${flexBetween} mx-auto w-5/6`}>
-                    <div className={`${flexBetween} w-full
-                     gap-16`}>
-                        {/* Left Side */}
-                        <img src={Logo} alt="Logo" style={{ width: '5rem' }} />
-                        {/* Right Side */}
-                        <div className={`${flexBetween} w-full`}>
-                            <div className={`${flexBetween} text-sm gap-8`}></div>
-                            <div className={`${flexBetween} text-sm gap-8`}></div>
-                        </div>
-                    </div>
+  return (
+    <nav>
+      <div
+        className={`${navbarBackground}${flexBetween} fixed top-0 z-30 w-full py-6`}
+      >
+        <div className={`${flexBetween} mx-auto w-5/6`}>
+          <div
+            className={`${flexBetween} w-full
+                     gap-16`}
+          >
+            {/* Left Side */}
+            <img src={Logo} alt="Logo" style={{ width: "5rem" }} />
+            {/* Right Side */}
+            {isAboveMediumScreens ? (
+              <div className={`${flexBetween} w-full`}>
+                <div className={`${flexBetween} gap-8 text-sm`}>
+                  <Link
+                    page="Home"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />
+                  <Link
+                    page="Benefits"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />
+                  <Link
+                    page="Our Classes"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />
+                  <Link
+                    page="Contact Us"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />
                 </div>
-            </div>
-        </nav >
-    )
-}
+                <div className={`${flexBetween} gap-8 `}>
+                  <p>Sign In</p>
+                  <ActionButton setSelectedPage={setSelectedPage}>
+                    Become a Member
+                  </ActionButton>
+                </div>
+              </div>
+            ) : (
+              <button
+                className="rounded-full bg-primary-500 p-2"
+                onClick={() => setIsMenuToggled(!isMenuToggled)}
+              >
+                <BookOpenIcon className="h-6 w-6 text-white " />
+              </button>
+            )}
+          </div>
+        </div>
+      </div>
+      {/* Mobile Menu Modal */}
+      {!isAboveMediumScreens && isMenuToggled && (
+        <div className="fixed right-0 bottom-0 z-40 h-full w-[300px] bg-primary-100 drop-shadow-xl">
+          {/* CLOSE ICON */}
+          <div className="flex justify-end p-12">
+            <button onClick={() => setIsMenuToggled(!isMenuToggled)}>
+              <ScissorsIcon className="h-6 w-6 text-gray-400" />
+            </button>
+          </div>
+          {/* MENU ITEMS */}
+          <div className="ml-[33%] flex flex-col gap-10 text-2xl">
+            <Link
+              page="Home"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Benefits"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Our Classes"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Contact Us"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+          </div>
+        </div>
+      )}
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
+// Finnished creating the navbar @144
